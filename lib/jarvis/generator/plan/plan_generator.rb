0d15c7237ad713a6ccfdc6eba33e88167ab9743b
Luis Benassi <luisfelipebenassi@gmail.com>
2016-10-13 17:04:43 -0300

    Plan generator and plan list to create plans on blaze

---
+++ lib/jarvis/generator/plan/plan_generator.rb
@@ -0,0 +1,44 @@
+require 'yaml'
+require 'blazemeter/blazemeter'
+
+
+module Generator
+  module Plan
+    class PlanGenerator
+      attr_accessor :file
+      attr_accessor :plan_id
+
+      def initialize(file_name)
+        @file = YAML.load_file("#{ENV['HOME']}/.jarvis/plan/#{file_name}.yml")
+      end
+
+      def create_on_blaze
+        @plan_id = Blaze.new.create_test(@file['plan'].to_json)
+        self.upload_files
+        return @plan_id
+      end
+
+      def upload_files
+        @file['files'].each do |key , path|
+          Blaze.new.upload_file(path, @plan_id)
+        end
+      end
+
+      def location(location_name=nil)
+        @file['plan']['configuration']['location'] = location_name if location_name
+        @file['plan']['configuration']['location']
+      end
+
+      def rampup(value=nil)
+        @file['plan']['configuration']['plugins']['jmeter']['override']['rampup'] = value if value
+        @file['plan']['configuration']['plugins']['jmeter']['override']['rampup']
+      end
+
+      def duration(value=nil)
+        @file['plan']['configuration']['plugins']['jmeter']['override']['duration'] = value if value
+        @file['plan']['configuration']['plugins']['jmeter']['override']['duration']
+      end
+
+    end
+  end
+end
---
+++ lib/jarvis/generator/plan/plan_list.rb
@@ -0,0 +1,27 @@
+require 'jarvis/generator/plan/plan_generator'
+
+module Generator
+  module Plan
+    class PlanList
+      attr_accessor :plan_list
+
+      def initialize(plans)
+        @plan_list = {}
+        plans.each do |plan|
+          @plan_list[plan] = Generator::Plan::PlanGenerator.new(plan)
+        end
+      end
+
+      def create_on_blaze
+        @plan_list.each do |name, plan|
+          plan.create_on_blaze
+        end
+      end
+
+      def names
+        @plan_list.keys
+      end
+
+    end
+  end
+end
