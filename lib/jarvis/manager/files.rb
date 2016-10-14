5dd8873d4f703843f0678910c645218a70205796
Cássio Kenji <chaoscifer@gmail.com>
2016-10-14 18:56:04 -0300

    fix some rubocop todos

--- lib/jarvis/manager/files.rb
+++ lib/jarvis/manager/files.rb
@@ -1,9 +1,10 @@
 
 module Manager
+  # top class
   class Files
 
     def list(path)
-      list = %x( ls #{ENV['HOME']}/.jarvis/#{path} )
+      list = `( ls #{ENV['HOME']}/.jarvis/#{path} )`
       list = list.split("\n")
       list.each do |item|
         item = item.slice! '.yml'
