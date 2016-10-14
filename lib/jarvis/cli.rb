b271ca3544fdfae3f22c0f74419b820bc76d422e
Cássio Kenji <chaoscifer@gmail.com>
2016-10-14 16:42:14 -0300

    fix some rubocop todos

--- lib/jarvis/cli.rb
+++ lib/jarvis/cli.rb
@@ -3,14 +3,12 @@ require 'jarvis/generator/multi/multi_generator'
 require 'jarvis/questions/menu'
 require 'helpers/printer'
 
-
 module Jarvis
+  # top class
   class Cli < Thor
-
     desc 'execute', 'Runs Jarvis wizard'
     def execute
       Questions::Menu.start
     end
-
   end
 end
