cadbe1957d1385d9bfefa7504246764582f2238a
Cássio Kenji <chaoscifer@gmail.com>
2016-10-14 18:11:28 -0300

    fix some rubocop todos

--- lib/jarvis/questions/menu.rb
+++ lib/jarvis/questions/menu.rb
@@ -4,23 +4,25 @@ require 'jarvis/questions/create_test'
 require 'artii'
 
 module Questions
+  # top class
   class Menu < Thor::Group
-      def navigation
-        system('clear')
 
-        a = Artii::Base.new :font => 'slant'
-        puts a.asciify('Jarvis').white.on_blue
+    def navigation
+      system('clear')
+      a = Artii::Base.new font: 'slant'
+      puts a.asciify('Jarvis').white.on_blue
 
-        menu = [
-          'Create test on BlazeMeter',
-          'Quit'
-        ]
-        menu_idx = Ask.list 'Menu', menu
-        case menu[menu_idx]
-        when 'Create test on BlazeMeter'
-            Questions::CreateTest.start
-        end
+      menu = [
+        'Create test on BlazeMeter',
+        'Quit'
+      ]
+      menu_idx = Ask.list 'Menu', menu
+
+      case menu[menu_idx]
+      when 'Create test on BlazeMeter'
+        Questions::CreateTest.start
       end
+    end
 
   end
 end
