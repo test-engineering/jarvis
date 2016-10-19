f881bda0e103deb0b6374a20590be064587f41a9
Luis Benassi <luisfelipebenassi@gmail.com>
2016-10-19 13:05:17 -0200

    fix rubocop offenses on mult generator

--- lib/jarvis/generator/multi/multi_generator.rb
+++ lib/jarvis/generator/multi/multi_generator.rb
@@ -4,6 +4,7 @@ require 'blazemeter/blazemeter'
 module Generator
   module Multi
     class MultiGenerator
+
       attr_accessor :plans
       attr_accessor :multi_id
       attr_accessor :body
@@ -12,14 +13,14 @@ module Generator
       def initialize(file_name)
         @config = YAML.load_file("#{ENV['HOME']}/.jarvis/multi/#{file_name}.yml")
         @plans = Generator::Plan::PlanList.new(@config['plans'].keys)
-        @body = {'name' => @config['name'], 'projectId' => @config['projectId'], 'items' => []}
+        @body = { 'name' => @config['name'], 'projectId' => @config['projectId'], 'items' => [] }
       end
 
       def create_on_blaze
         @plans.create_on_blaze
-        self.generate_items
-        self.set_rampup_duration(@config['rampup'], @config['duration'])
-        puts "#{@body['name']}".colorize(:light_blue)
+        generate_items
+        set_rampup_duration(@config['rampup'], @config['duration'])
+        puts @body['name'].to_s.colorize(:light_blue)
         response = Blaze.new.create_mult_test(@body.to_json)
         puts "https://a.blazemeter.com/app/#projects/#{response['result']['projectId']}/collections/#{response['result']['id']}".colorize(:light_blue).underline
       end
@@ -27,24 +28,24 @@ module Generator
       def generate_items
         @plans.names.each do |plan_name|
           if @config['plans'][plan_name]['copies']
-            self.add_item(plan_name, @config['plans'][plan_name]['copies'])
+            add_item(plan_name, @config['plans'][plan_name]['copies'])
           else
-            self.add_item(plan_name)
+            add_item(plan_name)
           end
         end
       end
 
       def add_item(plan_name, qtd = 1)
         qtd.times do
-          @body['items'] << {'testId' => @plans.plan_list[plan_name].plan_id,
-                             'location' => @plans.plan_list[plan_name].location,
-                             'override' => {'rampup' => @plans.plan_list[plan_name].rampup,
-                                            'duration' => @plans.plan_list[plan_name].duration}}
+          @body['items'] << { 'testId' => @plans.plan_list[plan_name].plan_id,
+                              'location' => @plans.plan_list[plan_name].location,
+                              'override' => { 'rampup' => @plans.plan_list[plan_name].rampup,
+                                              'duration' => @plans.plan_list[plan_name].duration } }
         end
       end
 
       def set_rampup_duration(rampup, duration)
-        fail 'Generate itens first...' if @body['items'] == []
+        raise 'Generate itens first...' if @body['items'] == []
         @body['items'].each do |item|
           item['override']['rampup'] = rampup * 60
           item['override']['duration'] = duration
